893d4d3467b4b18a4bbe4b74fc5aaac68dfbc6cb
Qiyue Hao <yue.hao031@gmail.com>
2025-08-10 00:25:03 +0800

    chore: wrong command line args messages

--- gitlet/Main.java
+++ gitlet/Main.java
@@ -15,25 +15,48 @@ public class Main {
         // Please enter a command.
         // and exit.
         if (args.length == 0) {
-            // call gitlet help?
-
+            Utils.message("Please enter a command.");
+            System.exit(0);
         }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                // TODO: handle the `init` command
+                validateNumArgs("init", args, 1);
 
                 break;
             case "add":
-                // TODO: handle the `add [filename]` command
+                validateNumArgs("add", args, 2);
                 break;
-            // TODO: FILL THE REST IN
             case "commit":
+
                 break;
+            // If a user inputs a command that doesn’t exist, print the message
+            // No command with that name exists.
+            // and exit.
+            default:
+                Utils.message("No command with that name exists.");
+                System.exit(0);
         }
     }
     public static void init() {
         Commit initial = new Commit("initial commit", null);
 
+    }
+
+    /**
+     * Checks the number of arguments versus the expected number,
+     * If a user inputs a command with the wrong number or format of operands, print the message
+     * Incorrect operands.
+     * and exit.
+     *
+     * @param cmd Name of command you are validating
+     * @param args Argument array from command line
+     * @param n Number of expected arguments
+     */
+    public static void validateNumArgs(String cmd, String[] args, int n) {
+        if (args.length != n) {
+            Utils.message("Incorrect operands.");
+            System.exit(0);
+        }
     }
 }
